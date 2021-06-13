61fba6a9b7ff021eaacd68e905dac53208b0aba1
GlauberRoque <glauberroqueferr@gmail.com>
2021-06-13 20:52:38 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroEmpresa/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroEmpresa.Models
+{
+    public class Contexto : DbContext
+    {
+        // Referencia para criação da base de dados.
+        public Contexto (DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Empresa> Empresas { get; set; }
+    }
+}
---
+++ CadastroEmpresa/Models/Empresa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroEmpresa.Models
+{
+
+    // Criação dos atributos e Nomeação das tabelas e campos do Banco de dados.
+    [Table("Empresa")]
+    public class Empresa
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nome Fantasia")]
+        [Column("Nome Fantasia")]
+        public string Nome_Fantasia { get; set; }
+
+        [Required]
+        [Display(Name = "Cnpj")]
+        [Column("Cnpj")]
+        public int Cnpj { get; set; }
+
+        [Required]
+        [Display(Name = "Telefone")]
+        [Column("Telefone")]
+        public int Telefone { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
+        [DataType(DataType.Date)]
+        [Column("Data de Cadastro")]
+        public string Data_Cadastro { get; set; }
+
+    }
+  
+}
